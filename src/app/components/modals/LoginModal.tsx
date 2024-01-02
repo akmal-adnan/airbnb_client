ff6294ebee38331540b25de0fa582a7fd4b448bb
Akmal Adnan <81947525+akmal-adnan@users.noreply.github.com>
2024-01-02 22:06:21 +0800

    feat: add github login

--- src/app/components/modals/LoginModal.tsx
+++ src/app/components/modals/LoginModal.tsx
@@ -81,15 +81,15 @@ function LoginModal() {
       <hr />
       <Button
         outline
-        label="Continue with Github"
-        icon={AiFillGithub}
+        label="Continue with Google"
+        icon={FcGoogle}
         onClick={() => {}}
       />
       <Button
         outline
-        label="Continue with Google"
-        icon={FcGoogle}
-        onClick={() => {}}
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => signIn('github')}
       />
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="justify-center text-center flex flex-row items-center gap-2">
