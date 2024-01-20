7b5419804feb33e5ce70142aa22f09606552a159
Akmal Adnan <81947525+akmal-adnan@users.noreply.github.com>
2024-01-21 01:00:11 +0800

    fix: Refactor file directory

---
+++ app/components/listings/ListingCard.tsx
@@ -0,0 +1,49 @@
+import useCountries from '@/app/hooks/useCountries';
+import { SafeUser } from '@/app/types';
+import { Listing, Reservation } from '@prisma/client';
+import { useRouter } from 'next/navigation';
+import React, { useCallback } from 'react';
+
+interface ListingCardProps {
+  data: Listing;
+  reservation?: Reservation;
+  onAction?: (id: string) => void;
+  disabled?: boolean;
+  actionLabel?: string;
+  actionId?: string;
+  currentUser?: SafeUser | null;
+}
+
+const ListingCard: React.FC<ListingCardProps> = ({
+  data,
+  reservation,
+  onAction,
+  disabled,
+  actionLabel,
+  actionId = '',
+  currentUser,
+}) => {
+  const router = useRouter;
+  const { getByValue } = useCountries();
+  const location = getByValue(data.locationValue);
+
+  const handleCancel = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation();
+
+      if (disabled) {
+        return;
+      }
+      onAction?.(actionId);
+    },
+    [disabled, onAction, actionId]
+  );
+
+  return (
+    <div>
+      <div className="">Listing cards</div>
+    </div>
+  );
+};
+
+export default ListingCard;
